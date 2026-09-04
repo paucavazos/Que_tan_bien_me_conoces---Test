df595d0fdc75553928199691d51053dc62ad8877
paucavazos <a01716693@tec.mx>
2026-09-03 22:29:55 -0600

    Delete Test - Código 1.py

--- Test - Código 1.py
+++
@@ -1,7 +0,0 @@
-preguntas = int(input("¿Cuántas preguntas tiene el test? "))
-correctas = int(input("¿Cuántas respuestas correctas obtuviste? "))
-
-calificacion = (correctas / preguntas) * 100
-
-print("Tu calificación es:", calificacion, "%")
-
